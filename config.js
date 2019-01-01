f3e9b62a0c1a8125ac01f68a16b345be92ff97cb
guitarYD <krauser085@gmail.com>
2019-01-01 21:55:09 +0900

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Yongdeok An";
+config.admin.email="";
+config.app.name="DiscoverySampleReact";
+config.app.url="";
+
+module.exports=config
