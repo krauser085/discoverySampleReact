9f312d4c4b4a85de6d15eb8e70b0242c8ad06f3a
guitarYD <krauser085@gmail.com>
2019-01-02 09:06:03 +0900

    add global css file

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import {render} from 'react-dom';
-
-import './../scss/main.scss';
-
-import User from './components/User.jsx';
+import { render } from 'react-dom';
 
 class App extends React.Component {
   render () {
     return (
-      <div className="home">
-        <User />
-        <p>Welcome to the ReactJS and ExpressJS generator</p>
-        <p>Check out the <a href="https://github.com/kevin-wynn/reactjs-express-generator/wiki">documentation</a> to get started.</p>
-      </div>
+      // <div className="home">
+      //   <User />
+      //   <p>Welcome to the ReactJS and ExpressJS generator</p>
+      //   <p>Check out the <a href="https://github.com/kevin-wynn/reactjs-express-generator/wiki">documentation</a> to get started.</p>
+      // </div>
+      <div>
+        <header>
+          <h2 className="g_container">Search Documents</h2>
+        </header>
+        <div class="g_container">
+        </div >
+      </div >
     );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
