0d4fd633ad8d672c704eb6b9cf22700515839a32
guitarYD <krauser085@gmail.com>
2019-01-03 23:06:14 +0900

    bind Form Component with event handlers

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -3,18 +3,38 @@ import React, { Component } from 'react';
 // style
 import '../../scss/components/form.scss'
 
+const tag = '[FormComponent]'
+
 class Form extends Component {
 
   constructor(props) {
     super(props)
+    this.state = { value: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
   render () {
     return (
-      <form>
-        <input type="text" className="g_border"></input>
-        <button type="reset">X</button>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" className="g_border"
+          onChange={this.handleChange}
+          placeholder="検索文を入力して下さい"
+        ></input>
+        <button type="reset" onClick={this.handleReset}>X</button>
       </form >
     )
+  }
+  handleChange(e) {
+    this.setState({value: e.target.value})
+    if (!e.target.value) this.handleReset()
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(tag, 'handleSubmit()', this.state.value)
+  }
+  handleReset() {
+    console.log(tag, 'handleReset()')
   }
 }
 
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,17 +7,12 @@ import SearchForm from './components/Form.jsx'
 class App extends React.Component {
   render () {
     return (
-      // <div className="home">
-      //   <User />
-      //   <p>Welcome to the ReactJS and ExpressJS generator</p>
-      //   <p>Check out the <a href="https://github.com/kevin-wynn/reactjs-express-generator/wiki">documentation</a> to get started.</p>
-      // </div>
       <div>
         <header>
           <h2 className="g_container">Search Documents</h2>
         </header>
         <div class="g_container">
-          <SearchForm />
+          <SearchForm query="this is test"/>
         </div >
       </div >
     );
